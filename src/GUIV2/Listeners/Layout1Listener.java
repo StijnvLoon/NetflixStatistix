01b8c42111534896bd247883052079cf2d1db9b0
StijnvLoon <stijnvanloon@home.nl>
2019-01-14 17:44:28 +0100

    sqlconnection in Layout1Listener voegen

--- src/GUIV2/Listeners/Layout1Listener.java
+++ src/GUIV2/Listeners/Layout1Listener.java
@@ -1,5 +1,6 @@
 package GUIV2.Listeners;
 
+import Database.SqlConnection;
 import GUIV2.UserInterface;
 
 import javax.swing.*;
@@ -9,14 +10,14 @@ import java.awt.event.ActionListener;
 public class Layout1Listener implements ActionListener {
 
     private UserInterface ui;
-    private String text;
+    private SqlConnection sqlConnection;
 
     public Layout1Listener(UserInterface ui) {
         this.ui = ui;
     }
 
-    public Layout1Listener(String text) {
-        this.text = text;
+    public Layout1Listener(SqlConnection sqlConnection) {
+        this.sqlConnection = sqlConnection;
     }
 
     @Override
@@ -25,6 +26,20 @@ public class Layout1Listener implements ActionListener {
         JComboBox cb = (JComboBox)e.getSource();
         String chosenFilm = (String)cb.getSelectedItem();
 
-        ui.changeLayout1(this.text);
+        if (chosenFilm.equals("Pirates of the Caribbean: The Curse of the Black Pearl")) {
+            ui.changeLayout1(getPirates());
+
+        } else if (chosenFilm.equals("Lord of the rings")) {
+            ui.changeLayout1(getRings());
+
+        }
+    }
+
+    private String getPirates() {
+
+    }
+
+    private String getRings() {
+
     }
 }
